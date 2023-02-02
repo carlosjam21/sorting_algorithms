3535c13af504869daaf33794d6863893c2b4a9f8
carlosjam21 <carlosalvarez29897@gmail.com>
2023-02-01 20:53:09 -0400

    Added README.md and Selection algorithm

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sorting Algorithms
+
+A collection of sorting algorithms made with php
--- Sort.php
+++ Sort.php
@@ -19,11 +19,48 @@ class Sort
         }
         return $numbers;
     }
+
+    public static function byCarlos(array $numbers){
+
+        for ($i=1; $i < count($numbers) ; $i++) { 
+            
+            for ($j=0; $j < $i  ; $j++) { 
+                
+                if( $numbers[$j] > $numbers[$i] ){
+                    $aux = $numbers[$j];
+
+                    $numbers[$j] = $numbers[$i];
+                    $numbers[$i] = $aux; 
+                }
+            }
+        }
+        return $numbers;
+    }
+
+    public static function bySelection(array $numbers){
+
+        for ($i=0; $i < count($numbers) - 1; $i++) { 
+            $minimo = $i;
+
+            for ($j= $i+1; $j < count($numbers); $j++) { 
+                
+                if( $numbers[$minimo] > $numbers[$j] ){
+                    $minimo = $j;
+                }
+            }
+
+            $aux = $numbers[$i];
+
+            $numbers[$i] = $numbers[$minimo];
+            $numbers[$minimo] = $aux;
+        }
+        return $numbers;
+    }
 }
 
 $arr = array(3,4,5,8,1,3,7,0,43,3,84,33,48,7);
 
-$arr = Sort::byBubble( $arr );
+$arr = Sort::bySelection( $arr );
 
 foreach( $arr as $value){
     echo $value . " , ";
